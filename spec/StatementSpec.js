67a83ff7d64063d0a71cae5f8e68c481832cc29c
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2017-11-24 05:48:40 -0800

    Added statement class

---
+++ spec/StatementSpec.js
@@ -0,0 +1,10 @@
+var Statement = require("../src/Account.js")
+
+'use strict';
+
+describe('Statement',function(){
+  var account;
+  beforeEach(function(){
+    statement = new Statement();
+  });
+});
