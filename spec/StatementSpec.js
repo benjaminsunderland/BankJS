3d32c6c6053a34aa3a2271ccd8f6964b2e300665
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2017-11-24 05:52:07 -0800

    Added test to check if a user is able to view their statement

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -1,10 +1,13 @@
-var Statement = require("../src/Account.js")
+var Statement = require("../src/Statement.js")
 
 'use strict';
 
 describe('Statement',function(){
-  var account;
+  var statement;
   beforeEach(function(){
     statement = new Statement();
+  });
+  it("is able for a user to see their statement", function() {
+    expect(statement.seeStatement()).toEqual([])
   });
 });
--- src/Statement.js
+++ src/Statement.js
@@ -1,3 +1,9 @@
 function Statement(){
-  
+  this._statement = []
 }
+
+Statement.prototype.seeStatement = function() {
+  return this._statement
+}
+
+module.exports = Statement;
