1836faf6f3ed44fad0a0f98292a422866f73ba46
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2017-11-24 05:20:23 -0800

    Added withdraw function with relevant test

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -7,8 +7,13 @@ describe('Account',function(){
   beforeEach(function(){
     account = new Account();
   });
-  it('is able to deposit', function(){
+  it('is able for a user to deposit', function(){
     account.deposit(40)
     expect(account.balance()).toEqual(40)
+  });
+  it('is able for a user to withdraw', function() {
+    account.deposit(40)
+    account.withdraw(20)
+    expect(account.balance()).toEqual(20)
   });
 });
--- src/Account.js
+++ src/Account.js
@@ -6,6 +6,10 @@ Account.prototype.deposit = function(amount) {
   this._balance += amount
 }
 
+Account.prototype.withdraw = function(amount) {
+  this._balance -= amount
+}
+
 Account.prototype.balance = function() {
   return this._balance
 }
