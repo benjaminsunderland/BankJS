58b5a224c5f62b0b437c4ecc4ac771cac9f28dc7
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2017-11-24 15:48:57 +0000

    Test statement module that it stores the log in correct format

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -10,4 +10,8 @@ describe('Statement',function(){
   it("is able for a user to see their statement", function() {
     expect(statement.seeStatement()).toEqual([])
   });
+  it("is able for the statement to store the history", function() {
+    statement.storeHistory(40, 40, "Deposit")
+    expect(statement.seeStatement()).toEqual([{amount: 40, balance: 40, type: "Deposit"}])
+  });
 });
